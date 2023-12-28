adc8230ca6b4b9cb44c0396105808a036f4abf34
iMaxZorky <mkoprav@gmail.com>
2023-12-28 18:22:15 +0600

    feat: shuffleChar done

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -368,8 +368,32 @@ function sortByAsc(/* arr */) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  let result = str;
+  let iter = iterations;
+  let step = 0; // считаю количество итераций, прежде чем результат сравняется с исходником, чтобы исключить повторные прогоны по кругу с к такому же результату
+  while (iter > 0) {
+    step += 1;
+    let left = '';
+    let right = '';
+    for (let i = 0; i < str.length; i += 2) {
+      if (result[i]) {
+        left += result[i];
+      }
+    }
+    for (let i = 1; i < str.length; i += 2) {
+      if (result[i]) {
+        right += result[i];
+      }
+    }
+    result = left + right;
+    if (result === str) {
+      iter = iterations % step; // убираю лишние итерации, которые приведут к исходной строке
+    } else {
+      iter -= 1;
+    }
+  }
+  return result;
 }
 
 /**
